59713468b12cbd80d7dd3b8e515a207a25f71f23
jrudio <justinjrudio@gmail.com>
2015-04-30 05:16:57 -0700

    Made test relevant with revised functions

--- test/test.js
+++ test/test.js
@@ -16,82 +16,38 @@ var fakeHeaders = {
 
 var headerError = /Missing\srequired\sheader\(s\)/g;
 
+var plexPin = new PlexPin(fakeHeaders);
 
-describe('does PlexPin return expected properties and methods?', function() {
+
+describe('Are there PlexPin Properties(Simple checks)?', function() {
   it('should be of type object', function(){
-    assert.equal(typeof new PlexPin(fakeHeaders), 'object');
+    assert.equal(typeof plexPin, 'object');
   });
 
-  it('should have property getPin', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('getPin'), true);
+  it('should have headers', function(){
+    assert.equal(plexPin.hasOwnProperty('headers'), true);
   });
 
-  it('should have property regEx', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('regEx'), true);
+  it('should have property regEx which in turn has pin, authToken, expireTime, & requestId as props', function(){
+    assert.equal(
+      !!( 
+        plexPin.hasOwnProperty('regEx')
+       && plexPin.regEx.hasOwnProperty('pin')
+       && plexPin.regEx.hasOwnProperty('authToken')
+       && plexPin.regEx.hasOwnProperty('expireTime')
+       && plexPin.regEx.hasOwnProperty('requestId')
+      )
+    , true);
   });
 
-  it('should have property headers', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('headers'), true);
-  });
-
-  it('should have property plexUrl', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('plexUrl'), true);
-  });
-
-  it('should have property token', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('token'), true);
-  });
-
-  it('should have property date', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('date'), true);
-  });
-
-  it('should have property code', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('code'), true);
-  });
-
-  it('should have property reqId', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('reqId'), true);
-  });
-
-  it('should have property test', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('getPin'), true);
-  });
-
-  it('should have method grabCode', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('grabCode'), true);
-  });
-
-  it('should have method grabDate', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('grabDate'), true);
-  });
-
-  it('should have method grabToken', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('grabToken'), true);
-  });
-
-  it('should have method grabReqId', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('grabReqId'), true);
-  });
-
-  it('should have method setReqId', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('setReqId'), true);
-  });
-
-  it('should have method setCode', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('setCode'), true);
-  });
-
-  it('should have method setToken', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('setToken'), true);
-  });
-
-  it('should have method setDate', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('setDate'), true);
-  });
-
-  it('should have method checkPin', function(){
-    assert.equal(new PlexPin(fakeHeaders).hasOwnProperty('checkPin'), true);
+  it('should have object plexUrl which has requestPin & checkPin as props', function(){
+    assert.equal(
+      !!( 
+        plexPin.hasOwnProperty('plexUrl')
+       && plexPin.plexUrl.hasOwnProperty('requestPin')
+       && plexPin.plexUrl.hasOwnProperty('checkPin')
+      )
+    , true);
   });
 });
 
@@ -99,36 +55,16 @@ describe('PlexPin', function() {
   it('should throw an error when required headers are not present', function(){
     assert.throws(function(){ return new PlexPin(); }, headerError);
   });
-});
 
-describe('getPin()', function() {
+  it('should set the pin, expireTime, and requestId when calling requestPin()', function() {
+    plexPin.requestPin().then(function(result){
+      plexPin.setPin(result);
+      plexPin.setExpireTime(result);
+      plexPin.setRequestId(result);
 
-/*  it('should return a promise', function() {
-    var plexPin = new PlexPin(fakeHeaders);
+
+      assert(!!(plexPin.getPin() && plexPin.getExpireTime() && plexPin.getRequestId()), true);
+    });
     
-    assert(plexPin.getPin().hasOwnProperty('then'), true);
-  });
-  */
-
-  it('should set the property code', function() {
-    var plexPin = new PlexPin(fakeHeaders);
-
-    plexPin.getPin().then(function(result){
-      var code = plexPin.grabCode(result);
-      plexPin.setCode(code);
-      assert.notEqual(typeof plexPin.code, null);
-    });
-
-  });
-
-  it('should set the property reqId', function() {
-    var plexPin = new PlexPin(fakeHeaders);
-
-    plexPin.getPin().then(function(result){
-      var reqId = plexPin.grabCode(result);
-      plexPin.setReqId(reqId);
-      assert.notEqual(plexPin.reqId, null);
-    });
-
   });
 });
